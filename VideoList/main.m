5f747401b7b982825a1d76853e5f56662b9ba78d
davvidja <davvidja@hotmail.com>
2014-04-04 19:18:43 +0200

    Initial Commit

---
+++ VideoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoList
+//
+//  Created by Carina Macia on 04/04/14.
+//  Copyright (c) 2014 different.read. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
