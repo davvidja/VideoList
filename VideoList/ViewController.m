0264020a39a811a3bc7afb5607c72354be010a7c
Raul <gomezriera@gmail.com>
2014-04-12 19:28:29 +0200

    Test query with Youtube returning JSON

--- VideoList/ViewController.m
+++ VideoList/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -21,6 +22,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // Create a service object for executing queries
+    GTLServiceYouTube *service = [[GTLServiceYouTube alloc]init];
+    
+    // Services which do not require sign-in may need an API key from the
+    // API Console
+    service.APIKey = @"AIzaSyA0d-WuKvwoa4GbkLfUzIOTRdYBsomp7FE";
+    
+    
+    GTLQueryYouTube *query = [GTLQueryYouTube queryForSearchListWithPart:@"id"];
+    query.q = @"La Sagrada Familia";
+    query.type = @"video";
+    query.safeSearch = @"strict";
+    query.videoDimension = @"2d";
+    query.maxResults = 10;
+   
+    // Execute the query
+    GTLServiceTicket *ticket = [service executeQuery:query
+                                   completionHandler:^(GTLServiceTicket *ticket, id object, NSError *error) {
+                                       // This callback block is run when the fetch completes
+                                       if (error == nil) {
+                                           GTLCollectionObject *products = object;
+                                           
+                                           // GTLShoppingProducts derives from GTLCollectionObject, so it supports
+                                           // iteration of items and subscript access to items.
+                                           for (GTLObject *item in products) {
+                                               // Print the name of each product result item
+                                               NSLog(@"%@", item.JSONString);
+                                           }
+                                       }
+                                   }];
+    
+    
     
     /*
     self.videoListDelegate = [[VideoListDelegate alloc] initWithMode:1];
