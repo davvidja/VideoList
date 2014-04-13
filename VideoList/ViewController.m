e73f17e6874cd941dbf120ce231a61afb62c2b48
Raul <gomezriera@gmail.com>
2014-04-13 17:56:40 +0200

    Sent to the console “video url”, “title” and “thumbnail"

--- VideoList/ViewController.m
+++ VideoList/ViewController.m
@@ -28,10 +28,12 @@
     
     // Services which do not require sign-in may need an API key from the
     // API Console
-    service.APIKey = @"AIzaSyA0d-WuKvwoa4GbkLfUzIOTRdYBsomp7FE";
+    service.APIKey = @"AIzaSyDQZOVtBYHAv-8CXSZZo0-JDUAt3gjSfzg";
+    // Enable fetches to continue in the background on iOS
+    service.shouldFetchInBackground = YES;
     
     
-    GTLQueryYouTube *query = [GTLQueryYouTube queryForSearchListWithPart:@"id"];
+    GTLQueryYouTube *query = [GTLQueryYouTube queryForSearchListWithPart:@"snippet"];
     query.q = @"La Sagrada Familia";
     query.type = @"video";
     query.safeSearch = @"strict";
@@ -48,12 +50,22 @@
                                            // GTLShoppingProducts derives from GTLCollectionObject, so it supports
                                            // iteration of items and subscript access to items.
                                            for (GTLObject *item in products) {
+                                            
                                                // Print the name of each product result item
-                                               NSLog(@"%@", item.JSONString);
+                                               NSLog(@"Video: https://www.youtube.com/v/%@?version=3&f=videos&app=youtube_gdata",[[item.JSON objectForKey:@"id"] objectForKey:@"videoId"]);
+                                               NSLog(@"Title: %@",[[item.JSON objectForKey:@"snippet"] objectForKey:@"title"]);
+                                               NSLog(@"thumbnails: %@",[[[[item.JSON objectForKey:@"snippet"] objectForKey:@"thumbnails"] objectForKey:@"medium"] objectForKey:@"url"]);
+                                               
+                                               //NSLog(@"Title:%@",[item.JSON objectForKey:@"snippet"]);
+                                               
+                                               
+                                               
                                            }
+
                                        }
                                    }];
     
+    NSLog(@"Service ticket has the status Code: %d",[ticket statusCode]);
     
     
     /*
